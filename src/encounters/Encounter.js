ec880b0b069c408ae37528ff52578eaaabfc1992
caseydulong <caseydulong@gmail.com>
2019-04-16 17:26:28 -0400

    Add combatant form show and hide

--- src/encounters/Encounter.js
+++ src/encounters/Encounter.js
@@ -1,33 +1,59 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
-// import axios from 'axios'
-// import apiUrl from '../apiConfig'
+import axios from 'axios'
+import apiUrl from '../apiConfig'
 
 class Encounter extends Component {
   constructor (props) {
     super(props)
     this.state = {
       user: this.props.user,
-      encounter: this.props.location.state.encounter
+      encounter: this.props.location.state.encounter,
+      newCombatantForm: false,
+      newCombatantName: '',
+      newCombatantInitiative: 0
     }
   }
 
-  newCombatant = () => {
-    const combatantsArray = this.state.encounter.combatants
-    combatantsArray.push({ name: '', initiative: 0 })
-    this.setState({
-      encounter: { combatants: combatantsArray }
+  newCombatantSubmit = event => {
+    event.preventDefault()
+    const { encounter, newCombatantName, newCombatantInitiative } = this.state
+    const combatants = encounter.combatants
+    combatants.push({
+      name: newCombatantName,
+      initiative: newCombatantInitiative
     })
-    console.log(this.state.encounter.combatants)
+    encounter.combatants = combatants.sort((a, b) => Number(b.initiative) - Number(a.initiative))
+    this.setState(this.state)
+
+    axios({
+      url: `${apiUrl}/encounters/${encounter._id}`,
+      method: 'patch',
+      headers: {
+        'Authorization': `Token token=${this.state.user.token}`
+      },
+      data: { encounter }
+    })
+      .then(() => console.log('PATCH COMPLETE'))
+      .catch(console.error)
+  }
+
+  handleChange = event => {
+    this.setState({ ...this.state, [event.target.name]: event.target.value })
+  }
+
+  newCombatant = () => {
+    this.setState({ newCombatantForm: true })
   }
 
   render () {
-    const { newCombatant } = this
+    const { newCombatant, newCombatantSubmit, handleChange } = this
+    const { newCombatantForm, newCombatantName, newCombatantInitiative } = this.state
 
     return (
       <div className="encounter-view">
         <section className="encounter-table">
-          <h2>Fight!</h2>
+          <h2>Encounter</h2>
           <table>
             <colgroup>
               <col width="80%" />
@@ -42,13 +68,35 @@ class Encounter extends Component {
             <tbody>
               {this.state.encounter.combatants.map(combatant => (
                 <tr key={combatant._id}>
+                  {console.log(combatant._id)}
                   <td>{combatant.name}</td>
                   <td className="init-col">{combatant.initiative}</td>
                 </tr>
               ))}
             </tbody>
           </table>
+          <p>Encounter ID: {this.state.encounter._id}</p>
         </section>
+        {newCombatantForm ? (
+          <section className="new-combatant">
+            <form onSubmit={newCombatantSubmit}>
+              <input
+                value={newCombatantName}
+                name="newCombatantName"
+                type="text"
+                placeholder="Name"
+                onChange={handleChange} />
+              <input
+                value={newCombatantInitiative}
+                name="newCombatantInitiative"
+                type="number"
+                min="1"
+                placeholder="Initiative"
+                onChange={handleChange} />
+              <button type="submit">Add Combatant</button>
+            </form>
+          </section>
+        ) : ''}
         <section className="encounter-controls">
           <i className="fas fa-plus control-button" onClick={newCombatant}></i>
           <i className="fas fa-skull control-button"></i>
